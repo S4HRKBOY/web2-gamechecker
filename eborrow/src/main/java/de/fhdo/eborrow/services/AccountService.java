b7521ca5ed14a65911d1406234e7122c5c011842
zabet001 <zakaria.bettahar001@stud.fh-dortmund.de>
2024-11-10 20:04:25 +0100

    Bugfix in transferChanges

--- eborrow/src/main/java/de/fhdo/eborrow/services/AccountService.java
+++ eborrow/src/main/java/de/fhdo/eborrow/services/AccountService.java
@@ -77,35 +77,37 @@ public class AccountService {
 	}
 
 	private AccountDto transferChanges(AccountDto existingAccountDto, AccountDto accountChanges) {
-		if (accountChanges.getPrename() == null) {
+		if (accountChanges.getPrename() != null) {
 			existingAccountDto.setPrename(accountChanges.getPrename());
 		}
 
-		if (accountChanges.getSurname() == null) {
+		if (accountChanges.getSurname() != null) {
 			existingAccountDto.setSurname(accountChanges.getSurname());
 		}
 
-		if (accountChanges.getUsername() == null) {
+		if (accountChanges.getUsername() != null) {
 			existingAccountDto.setUsername(accountChanges.getUsername());
 		}
 
-		if (accountChanges.getBirthday() == null) {
+		if (accountChanges.getBirthday() != null) {
 			existingAccountDto.setBirthday(accountChanges.getBirthday());
 		}
 
-		if (accountChanges.getEmail() == null) {
+		if (accountChanges.getEmail() != null) {
 			existingAccountDto.setEmail(accountChanges.getEmail());
 		}
 
-		if (accountChanges.getPassword() == null) {
+		if (accountChanges.getPassword() != null) {
 			existingAccountDto.setPassword(accountChanges.getPassword());
 		}
 
-		if (accountChanges.getProfilePicture() == null) {
+		if (accountChanges.getProfilePicture() != null) {
 			existingAccountDto.setProfilePicture(accountChanges.getProfilePicture());
 		}
 
-		if (accountChanges.getTaggedGames() == null) {
+		// Zak: Sollte der Wechsel des Status auf Publisher bzw. User unterstuetzt werden?
+		
+		if (accountChanges.getTaggedGames() != null) {
 			existingAccountDto.setTaggedGames(accountChanges.getTaggedGames());
 		}
 		
