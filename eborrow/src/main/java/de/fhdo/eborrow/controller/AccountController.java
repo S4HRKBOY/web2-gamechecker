a2643be6aeae64576be2e67b821dd66776cf88b8
zabet001 <zakaria.bettahar001@stud.fh-dortmund.de>
2024-11-20 20:27:02 +0100

    Added AccountController and profile_page thymeleaf template

---
+++ eborrow/src/main/java/de/fhdo/eborrow/controller/AccountController.java
@@ -0,0 +1,66 @@
+package de.fhdo.eborrow.controller;
+
+import de.fhdo.eborrow.dto.AccountDto;
+import de.fhdo.eborrow.dto.GameDto;
+import de.fhdo.eborrow.services.AccountService;
+import de.fhdo.eborrow.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/thymeleaf/account")
+public class AccountController {
+	private AccountService accountService;
+	private GameService gameService;
+
+	@Autowired
+	public AccountController(AccountService accountService, GameService gameService) {
+		this.accountService = accountService;
+		this.gameService = gameService;
+	}
+
+	@GetMapping("/{id}")
+	public String getAccountById(@PathVariable Long id, Model model) {
+		AccountDto accountDto = accountService.getAccountById(id);
+		List<GameDto> gamesDtos = accountDto.getTaggedGames();
+
+		model.addAttribute("account", accountDto);
+		model.addAttribute("games", gamesDtos);
+
+		return "profile_page";
+	}
+
+	@GetMapping("/{id}/edit")
+	public String showEditAccountView(@PathVariable Long id, Model model) {
+		AccountDto accountDto = accountService.getAccountById(id);
+		model.addAttribute("account", accountDto);
+
+		return "profile_edit_page";
+	}
+
+	@PostMapping("/{id}/edit")
+	public String updateAccount(@PathVariable Long id, @RequestBody AccountDto prefilledAccount, Model model) {
+		if (prefilledAccount == null) {
+			// TODO: Redirect to /{id}/edit
+			return showEditAccountView(id, model);
+		}
+
+		boolean updateSucceeded = accountService.updateAccount(prefilledAccount, id);
+		if (!updateSucceeded) {
+			// TODO: Redirect to /{id}/edit
+			return showEditAccountView(id, model);
+		}
+
+		AccountDto accountDto = accountService.getAccountById(id);
+		List<GameDto> gamesDtos = accountDto.getTaggedGames();
+
+		model.addAttribute("account", accountDto);
+		model.addAttribute("games", gamesDtos);
+
+		return "profile_page";
+	}
+}
