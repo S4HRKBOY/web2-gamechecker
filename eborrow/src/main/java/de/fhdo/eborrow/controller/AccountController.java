feeee6c7b94470dba43d7db5d45139a36e496067
zabet001 <zakaria.bettahar001@stud.fh-dortmund.de>
2024-11-24 14:29:19 +0100

    Reverted account route prefix to /thymeleaf/account for consistency

--- eborrow/src/main/java/de/fhdo/eborrow/controller/AccountController.java
+++ eborrow/src/main/java/de/fhdo/eborrow/controller/AccountController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/account")
+@RequestMapping("/thymeleaf/account")
 public class AccountController {
 	private final AccountService accountService;
 	private final GameService gameService;
