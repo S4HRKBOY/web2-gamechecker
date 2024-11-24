835cba1fcaad0d95258b1745c1baa93e86c97a82
Mareike Chudaska <hannah.chudaska001@stud.fh-dortmund.de>
2024-11-24 18:25:27 +0100

    small bugfix

--- eborrow/src/main/java/de/fhdo/eborrow/repositories/AccountRepository.java
+++ eborrow/src/main/java/de/fhdo/eborrow/repositories/AccountRepository.java
@@ -7,6 +7,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface AccountRepository extends CrudRepository<Account, Long> {
+    //native SQL Query 
     @Query(value = "SELECT COUNT(*) > 0 FROM account_game ag WHERE ag.account_id = :accountId AND ag.game_id = :gameId", nativeQuery = true)
     boolean accountHasGame(@Param("accountId") Long accountId, @Param("gameId") Long gameId);
+
+    //JPQL QUERY 
+    // @Query("SELECT CASE WHEN COUNT(g) > 0 THEN true ELSE false END FROM Account a JOIN a.taggedGames g WHERE a.id = :accountId AND g.id = :gameId")
+    // boolean accountHasGame(@Param("accountId") Long accountId, @Param("gameId") Long gameId); 
 }
