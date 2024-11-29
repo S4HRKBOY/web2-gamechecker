39efbd38a32fcb669c5bf190d6f1f0fe2728af66
Czekalla, David <David.Czekalla@adesso.de>
2024-11-29 13:37:18 +0100

    Thymeleaf Template erweitert mit Filterung und Suche

--- eborrow/src/main/java/de/fhdo/eborrow/restapi/ReviewRestController.java
+++ eborrow/src/main/java/de/fhdo/eborrow/restapi/ReviewRestController.java
@@ -17,10 +17,8 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
