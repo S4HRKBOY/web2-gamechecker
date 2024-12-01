3be3ce3cf94dfc72a0618fa0302fcda92364b44b
zabet001 <zakaria.bettahar001@stud.fh-dortmund.de>
2024-12-01 19:14:26 +0100

    Added GraphQL endpoint for admin

--- eborrow/src/main/java/de/fhdo/eborrow/graphqlapi/AdminGraphClController.java
+++ eborrow/src/main/java/de/fhdo/eborrow/graphqlapi/AdminGraphClController.java
@@ -1,8 +1,25 @@
 package de.fhdo.eborrow.graphqlapi;
 
+import de.fhdo.eborrow.dto.AccountDto;
+import de.fhdo.eborrow.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class AdminGraphClController {
-	// TODO Zak: Implementieren
+	private final AccountService accountService;
+
+	@Autowired
+	public AdminGraphClController(AccountService accountService) {
+		this.accountService = accountService;
+	}
+
+	@MutationMapping("setPublisherStatus")
+	public AccountDto setPublisherStatus(@Argument Long id, @Argument boolean isPublisher) {
+		boolean succeeded = accountService.updatePublisherStatus(id, isPublisher);
+
+		return accountService.getAccountById(id);
+	}
 }
