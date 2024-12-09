cc604b9d1503aa914f7648a69dc7352fb1728bb0
zabet001 <zakaria.bettahar001@stud.fh-dortmund.de>
2024-12-09 20:33:06 +0100

    Added file structure for frontend and global files

---
+++ eborrow-frontend/app-v1/js/global.js
@@ -0,0 +1,39 @@
+import {loadProfileEditPage} from "./profileEditPage.js";
+
+const idAccountToFetch = 1;
+
+profileEditPage();
+
+function homePage() {
+    // ...
+}
+
+function profileEditPage() {
+    loadProfileEditPage(idAccountToFetch);
+}
+
+function gameForm() {
+    // ...
+}
+
+export function removeCSSTags() {
+    // remove all existing stylesheet imports from the head
+    const links = document.querySelectorAll("link[rel=stylesheet]");
+    links.forEach((link) => link.remove());
+}
+
+export function createHeader(account) {
+    const newGameBtn = account.publisher ? `<li><a href="TODO">Neues Spiel anlegen</a></li>` : "";
+
+    return `<header>
+        <img class="logo" src="../resources/images/logo.svg" alt="logo">
+        <nav class="navigation">
+            <ul class="nav-links">
+                <li><a href="//localhost:8080/thymeleaf/home">Home</a></li>
+                ${newGameBtn}
+                <li><a href="//localhost:8080/thymeleaf/account/${account.id}">Profil</a></li>
+                <li><a href="TODO">Logout</a></li>
+            </ul>
+        </nav>
+    </header>`;
+}
