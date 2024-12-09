d000601e9614780b4a2996fe1cd6e2652750dfbf
zabet001 <zakaria.bettahar001@stud.fh-dortmund.de>
2024-12-09 20:31:19 +0100

    Added a Filter, that allows for CORS requests from frontend

--- eborrow/src/main/java/de/fhdo/eborrow/EborrowApplication.java
+++ eborrow/src/main/java/de/fhdo/eborrow/EborrowApplication.java
@@ -2,12 +2,30 @@ package de.fhdo.eborrow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class EborrowApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EborrowApplication.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		UrlBasedCorsConfigurationSource source
+				= new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean<CorsFilter> bean
+				= new FilterRegistrationBean<>(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
+	}
 }
