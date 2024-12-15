a760f78294be42655fd9b2394ad8fdd10bfad166
zabet001 <zakaria.bettahar001@stud.fh-dortmund.de>
2024-12-15 18:50:02 +0100

    Backend: Added exception handling for RestControllers and adapted GraphQL Exception handling

--- eborrow/src/main/java/de/fhdo/eborrow/graphqlapi/CustomGraphQlExceptionHandler.java
+++ eborrow/src/main/java/de/fhdo/eborrow/graphqlapi/CustomGraphQlExceptionHandler.java
@@ -2,38 +2,51 @@ package de.fhdo.eborrow.graphqlapi;
 
 import graphql.GraphQLError;
 import graphql.GraphqlErrorBuilder;
+import javassist.NotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.graphql.data.method.annotation.GraphQlExceptionHandler;
 import org.springframework.graphql.execution.ErrorType;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.server.ResponseStatusException;
 
 @ControllerAdvice
 public class CustomGraphQlExceptionHandler{
-	// Zak TODO: Handler schreiben, der abzufangende Exceptions zu einer ResponseStatusException umwandelt
-	// u.a. MethodArgumentNotValidException und DataIntegrityViolationException
-
 	@GraphQlExceptionHandler
-	public GraphQLError handleResponseStatusException(ResponseStatusException ex) {
-		GraphqlErrorBuilder<? extends GraphqlErrorBuilder<?>> error = GraphqlErrorBuilder.newError();
-		String reason = ex.getReason();
-		error.message(reason != null ? reason : "");
-
-		int value = ex.getStatusCode().value();
-
-		if (value == HttpStatus.BAD_REQUEST.value()) {
-			error.errorType(ErrorType.BAD_REQUEST);
-		} else if (value == HttpStatus.UNAUTHORIZED.value()) {
-			error.errorType(ErrorType.UNAUTHORIZED);
-		} else if (value == HttpStatus.FORBIDDEN.value()) {
-			error.errorType(ErrorType.FORBIDDEN);
-		} else if (value == HttpStatus.NOT_FOUND.value()) {
-			error.errorType(ErrorType.NOT_FOUND);
-		} else if (value == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-			error.errorType(ErrorType.INTERNAL_ERROR);
+	public GraphQLError mapToResponseStatusException(Exception ex) {
+		HttpStatus status;
+		String message;
+		if (ex instanceof NotFoundException) {
+			status = HttpStatus.NOT_FOUND;
+			message = ex.getMessage();
+		} else if (ex instanceof MethodArgumentNotValidException) {
+			status = HttpStatus.BAD_REQUEST;
+			message = "Validation failed: " + ex.getMessage();
+		} else if (ex instanceof DataIntegrityViolationException) {
+			status = HttpStatus.CONFLICT;
+			message = "Data integrity violation: " + ex.getMessage();
+		} else {
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+			message = "Unexpected error: " + ex.getMessage();
 		}
 
-		return error.build();
+		return GraphqlErrorBuilder.newError().message(message).errorType(httpStatusToErrorType(status)).build();
+	}
+
+	private ErrorType httpStatusToErrorType(HttpStatus status) {
+		if (status == HttpStatus.BAD_REQUEST) {
+			return ErrorType.BAD_REQUEST;
+		} else if (status == HttpStatus.UNAUTHORIZED) {
+			return ErrorType.UNAUTHORIZED;
+		} else if (status == HttpStatus.FORBIDDEN) {
+			return ErrorType.FORBIDDEN;
+		} else if (status == HttpStatus.NOT_FOUND) {
+			return ErrorType.NOT_FOUND;
+		} else if (status == HttpStatus.CONFLICT) {
+			return ErrorType.FORBIDDEN;
+		} else {
+			return ErrorType.INTERNAL_ERROR;
+		}
 	}
 }
 
