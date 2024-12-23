dbc0ec0d7a7183422ddcacd1d75945420500ee9e
zabet001 <zakaria.bettahar001@stud.fh-dortmund.de>
2024-12-23 21:34:50 +0100

    Frontend: Using GraphQL in header.js

--- eborrow-frontend/app-v1/js/pages/partials/header.js
+++ eborrow-frontend/app-v1/js/pages/partials/header.js
@@ -1,8 +1,10 @@
-import * as accountController from "../../controller/accountRestController.js";
+import * as accountController from "../../controller/accountGraphQLController.js";
 import { ID_ACCOUNT_TO_FETCH } from "../../global.js";
 
-accountController.getAccountById(ID_ACCOUNT_TO_FETCH)
-    .then(renderHeader);
+accountController.getAccountById(ID_ACCOUNT_TO_FETCH, [
+    "id",
+    "publisher"
+]).then(renderHeader);
 
 export function renderHeader(account) {
     const header = document.querySelector("header");
@@ -16,7 +18,7 @@ function createHeader(account) {
                 <li><a href="../html/start_page.html">Home</a></li>
                 ${account.publisher ? `<li><a href="gameForm.html">Neues Spiel anlegen</a></li>` : ""}
                 <li><a href="//localhost:8080/thymeleaf/account/${account.id}">Profil</a></li>
-                <li><a href="TODO">Logout</a></li>
+                <li><a href="#">Logout</a></li>
             </ul>
         </nav>`;
 }
