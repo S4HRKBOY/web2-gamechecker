fe6baa10ba22c55ca7d39c6573444dbb93d48eb2
zabet001 <zakaria.bettahar001@stud.fh-dortmund.de>
2024-12-23 19:52:40 +0100

    Frontend: Header button home now loads the start_page from app-v1

--- eborrow-frontend/app-v1/js/pages/partials/header.js
+++ eborrow-frontend/app-v1/js/pages/partials/header.js
@@ -13,7 +13,7 @@ function createHeader(account) {
     return `<img class="logo" src="../resources/images/logo.svg" alt="logo">
         <nav class="navigation">
             <ul class="nav-links">
-                <li><a href="//localhost:8080/thymeleaf/home">Home</a></li>
+                <li><a href="../html/start_page.html">Home</a></li>
                 ${account.publisher ? `<li><a href="gameForm.html">Neues Spiel anlegen</a></li>` : ""}
                 <li><a href="//localhost:8080/thymeleaf/account/${account.id}">Profil</a></li>
                 <li><a href="TODO">Logout</a></li>
